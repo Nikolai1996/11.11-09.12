9b13f6c0b89c630322c96e72ea8289a3f1466e8c
Nikolai <nikolay.golovachev96@mail.ru>
2022-11-16 15:50:48 +0300

    Add Task01/03/05/07

---
+++ Task07/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая 
+// 1. принимает на вход трёхзначное число и 
+// 2. на выходе показывает последнюю цифру этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 1000)
+{
+    number = Math.Abs(number);
+    number = number % 10;
+    Console.WriteLine(number);
+}
+else
+{
+    Console.WriteLine("Ошибочное значение");
+}
